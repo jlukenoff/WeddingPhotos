ff4ea3d923e7a917dce81e4b911b65d9d86711d1
John Lukenoff <jlukenoff@gmail.com>
2021-08-06 12:38:21 -0700

    Front end UI complete

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,16 @@
-import * as functions from 'firebase-functions';
+import * as functions from "firebase-functions";
+import * as express from "express";
+import * as admin from "firebase-admin";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const app = express();
+
+admin.initializeApp();
+
+/* app.use((req, res, next) => {
+  console.log(req.url);
+  next();
+});
+ */
+app.use(express.static("public"));
+
+exports.photo_upload = functions.https.onRequest(app);
